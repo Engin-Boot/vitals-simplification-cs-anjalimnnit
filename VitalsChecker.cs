3313ba3fe3d5e289b63595bb1049f460af8232d4
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-08-27 08:50:07 -0700

    displayed abnormal vitals

--- VitalsChecker.cs
+++ VitalsChecker.cs
@@ -9,11 +9,11 @@ class VitalsChecker
     { 
          if(bpm < 70)
          {  
-             console.WriteLine("Beat per heart is less than 70");
+             Console.WriteLine("Beat per heart is less than 70");
              return true;
          } 
          if( bpm > 150)
-    {   console.WriteLine("Beat per heart is greater than 150");
+    {   Console.WriteLine("Beat per heart is greater than 150");
         return true;
         
     }
@@ -23,7 +23,7 @@ class VitalsChecker
     static bool checkspo2(float spo2)
     { 
          if(spo2 < 90)
-         { console.WriteLine("Oxygen Saturation level is less than 90");
+         { Console.WriteLine("Oxygen Saturation level is less than 90");
          return true;
         
          }
@@ -33,12 +33,12 @@ class VitalsChecker
     { 
          if(respRate < 30)
          {
-              console.WriteLine("Respiration Rate level is less than 30");
+              Console.WriteLine("Respiration Rate level is less than 30");
             return true;
          } 
          if(respRate > 95)
          {
-              console.WriteLine("Respiration Rate  level is greater than 95");
+              Console.WriteLine("Respiration Rate  level is greater than 95");
               return true;
          }
          return false;
