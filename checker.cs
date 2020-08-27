17d03769fd7b8a2d81d2152400c6d9e22cff1795
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-08-27 07:50:30 -0700

    updated Checker

--- checker.cs
+++ checker.cs
@@ -17,14 +17,14 @@ class Checker
         }
     }
     static int Main() {
-        ExpectTrue(VitalsChecker.vitalsAreOk(100, 95, 60));
-        ExpectFalse(VitalsChecker.vitalsAreOk(40, 91, 92));
-        ExpectFalse(VitalsChecker.vitalsAreOk(151, 92, 100));
-        ExpectFalse(VitalsChecker.vitalsAreOk(72, 80, 92));
-        ExpectFalse(VitalsChecker.vitalsAreOk(80, 92, 20));
-        ExpectFalse(VitalsChecker.vitalsAreOk(80, 92, 100));
-		ExpectTrue(VitalsChecker.vitalsAreOk(70, 92, 95));
-		ExpectTrue(VitalsChecker.vitalsAreOk(150, 90, 30));
+        ExpectTrue(VitalsChecker.vitalsAreOk(100, 95, 60)); //(1,1,1,)
+        ExpectFalse(VitalsChecker.vitalsAreOk(40, 91, 92)); //(0,1,1)
+        ExpectFalse(VitalsChecker.vitalsAreOk(151, 92, 94)); //(0,1,1)
+        ExpectFalse(VitalsChecker.vitalsAreOk(72, 80, 92));  //(1,0,1)
+        ExpectFalse(VitalsChecker.vitalsAreOk(80, 92, 20)); //(1,1,0)
+        ExpectFalse(VitalsChecker.vitalsAreOk(80, 92, 100)); //(1,1,0)
+		ExpectTrue(VitalsChecker.vitalsAreOk(70, 92, 95));  //(1,1,1)
+		ExpectTrue(VitalsChecker.vitalsAreOk(150, 90, 30));  //(1,1,1)
 
         Console.WriteLine("All ok");
         return 0;
